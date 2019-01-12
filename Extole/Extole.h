50b5154dfa4a8b4803eb8f656de1605d24f6a4c8
Jordan Reed <2756092+chanomie@users.noreply.github.com>
2019-01-12 15:07:50 -0800

    Initial Commit

---
+++ Extole/Extole.h
@@ -0,0 +1,19 @@
+//
+//  Extole.h
+//  Extole
+//
+//  Created by Jordan Reed on 1/12/19.
+//  Copyright © 2019 Extole, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Extole.
+FOUNDATION_EXPORT double ExtoleVersionNumber;
+
+//! Project version string for Extole.
+FOUNDATION_EXPORT const unsigned char ExtoleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Extole/PublicHeader.h>
+
+
